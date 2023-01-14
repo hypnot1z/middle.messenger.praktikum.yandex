d21574b574da2d8dabd1872a1348fcdf7c84a610
Aleksei Zagorskii <m1nerall@yandex.ru>
2023-01-14 14:18:39 +0300

    add profile page

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ import Registr from './src/components/pages/Registration'
 import NotFound from './src/components/pages/NotFound'
 import ServerError from './src/components/pages/ServerError'
 import Chat from './src/components/pages/Chat'
+import Profile from './src/components/pages/Profile'
 import Button from './src/components/UI/Button'
 import styles from './main.scss'
+import ProfileEdit from './src/components/pages/ProfileEdit'
+import EditPassword from './src/components/pages/EditPassword'
 
 const root = document.getElementById('root')
 
@@ -24,8 +27,11 @@ const routes = {
   '/': Login,
   '/login': Login,
   '/registration': Registr,
+  '/profile': Profile,
+  '/editprofile': ProfileEdit,
   '/chat': Chat,
   '/500': ServerError,
+  '/editpassword': EditPassword,
 }
 
 // Функция отрисовки страницы
