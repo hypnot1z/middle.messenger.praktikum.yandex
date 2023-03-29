f474830ce65b1cf539863046c1c756eadf662790
Aleksei Zagorskii <m1nerall@yandex.ru>
2023-03-29 14:23:51 +0300

    upd

--- index.ts
+++ index.ts
@@ -6,26 +6,15 @@ import Profile from './src/components/pages/Profile'
 // import ProfileEdit from './src/components/pages/ProfileEdit'
 import EditProfile from './src/components/pages/ProfileEdit'
 import EditPassword from './src/components/pages/EditPassword'
-import { PageErr } from './src/components/pages/ErrorPage'
+// import { PageErr } from './src/components/pages/ErrorPage'
 import Router from './src/utils/Router'
 import AuthController from './src/controllers/AuthController'
 import store from './src/utils/Store'
 
-const NotFound = new PageErr({ code: '404', text: 'Не туда попали' })
-const ServerError = new PageErr({ code: '500', text: 'Мы уже фиксим' })
+// const NotFound = new PageErr({ code: '404', text: 'Не туда попали' })
+// const ServerError = new PageErr({ code: '500', text: 'Мы уже фиксим' })
 
-// Router
-// Маршруты
-// const Routes: Record<string, any> = {
-//   '/': Login,
-//   '/login': Login,
-//   '/registration': Registr,
-//   '/profile': Profile,
-//   '/editprofile': ProfileEdit,
-//   '/chat': Chat,
-//   '/500': ServerError,
-//   '/editpassword': EditPassword,
-// }
+
 
 enum Routes {
   Index = '/',
@@ -37,36 +26,7 @@ enum Routes {
   EditPassword = '/password',
 }
 
-// Render
-// function render(block: any) {
-//   const root: HTMLElement | null = document.querySelector('.app')
-//   root!.innerHTML = ''
-//   root!.append(block.getContent())
-//   return root
-// }
 
-// Функция отрисовки страницы
-// function route(url: string) {
-//   if (url in routes) {
-//     for (let key in routes) {
-//       if (key === url) {
-//         render(routes[key])
-//       }
-//     }
-//   } else {
-//     render(NotFound)
-//   }
-// }
-
-// Функция определения маршрута
-// function router() {
-//   let url = window.location.hash.slice(1) || '/'
-//   route(url)
-// }
-
-// Слушатели событий
-// window.addEventListener('load', router)
-// window.addEventListener('hashchange', router)
 
 window.addEventListener('DOMContentLoaded', async () => {
   Router.use(Routes.Index, Login)
@@ -79,12 +39,7 @@ window.addEventListener('DOMContentLoaded', async () => {
 
   let isProtectedRoute = true
 
-  // switch (window.location.pathname) {
-  //   case Routes.Index:
-  //   case Routes.Registr:
-  //     isProtectedRoute = false
-  //     break
-  // }
+ 
 
   try {
     await AuthController.fetchUser()
